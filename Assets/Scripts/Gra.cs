2635e4884a6286d1ef3e9ba556504910633bee28
daglas140 <sebastianhrb@gmail.com>
2017-04-24 18:37:44 +0200

    Dodałem kolizje z planszą
    Brak kolizji przy obrocie klocka

--- Assets/Scripts/Gra.cs
+++ Assets/Scripts/Gra.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Gra : MonoBehaviour {
 
+    public static int wysokośćPlanszy = 20;
+    public static int szerokośćPlanszy = 10;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +15,13 @@ public class Gra : MonoBehaviour {
 	void Update () {
 		
 	}
+    public bool sprawdzCzyJestWPlanszy(Vector3 pozycja)
+    {
+        return ((int) pozycja.x >= 0 && (int)pozycja.x < szerokośćPlanszy && (int)pozycja.y >= 0);
+    }
+
+    public Vector2 Round (Vector2 pozycja)
+    {
+        return new Vector2 (Mathf.Round(pozycja.x), Mathf.Round(pozycja.y));
+    }
 }
--- Assets/Scripts/Klocek.cs
+++ Assets/Scripts/Klocek.cs
@@ -4,35 +4,70 @@ using UnityEngine;
 
 public class Klocek : MonoBehaviour {
 
-	float fall=0;
-	public float fallSpeed=1;
-	// Use this for initialization
-	void Start () {
-		
+    float fall = 0;
+    public float fallSpeed = 1;
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Sterowanie ();
+        Sterowanie();
 	}
-	void Sterowanie()
-	{
-		if(Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			transform.position += new Vector3 (1, 0, 0);	
-		}
-		else if (Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			transform.position += new Vector3 (-1, 0, 0);			
-		}
-		else if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			transform.Rotate (0, 0, 90);
-		}
-		else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
-		{
-			transform.position += new Vector3 (0, -1, 0);	
-			fall = Time.time;
-		}
-	}
+    public void Sterowanie()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (sprawdzCzyJestWDobrejPozycji())
+            {
+
+            }else
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (sprawdzCzyJestWDobrejPozycji())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, 0, 90);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+        {
+            transform.position += new Vector3(0,-1,0);
+            if (sprawdzCzyJestWDobrejPozycji())
+            {
+
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+            fall = Time.time;
+        }
+    }bool sprawdzCzyJestWDobrejPozycji()
+    {
+        foreach (Transform klocek in transform)
+        {
+            Vector2 pozycja = FindObjectOfType<Gra>().Round(klocek.position);
+            if (FindObjectOfType<Gra>().sprawdzCzyJestWPlanszy(pozycja) == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
