728e14ad724ac0221c5df2a2bc1566694b55f812
oktawiuszm <oktawiusz.milo@gmail.com>
2017-04-25 08:40:43 +0200

    zeby nie przechodzili przez siebie itd w spadaniu rotacji itd ;p
    dokladnie tak jak napisalem wyzej

--- Assets/Scripts/Gra.cs
+++ Assets/Scripts/Gra.cs
@@ -2,40 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gra : MonoBehaviour {
+public class Gra : MonoBehaviour
+{
 
     public static int wysokośćPlanszy = 20;
     public static int szerokośćPlanszy = 10;
+    public static Transform[,] grid = new Transform[szerokośćPlanszy, wysokośćPlanszy];
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         spawnNowegoKlocka();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 
-    public void spawnNowegoKlocka()
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void aktualizowanieSiatki(Klocek klocek) { //chyba dobrze xd
+      for (int y = 0; y <wysokośćPlanszy; ++y) {
+        for (int x = 0; y <szerokośćPlanszy; ++x) {
+            if(grid[x,y] !=null) {
+            if (grid[x,y].parent == klocek.transform) {
+        grid[x,y] = null;
+            }
+          }
+        }
+      }
+    foreach (Transform kloc in klocek.transform){ //nie bic mnie za kloca;
+    Vector2 poz = Round (kloc.pozycja);
+        if (poz.y < wysokośćPlanszy) {
+            grid[(int)poz.x, (int)poz.y] = kloc;
+        }
+      }
+    
+    }
+    public Transform GetTransformAtGridPosition (Vector2 poz) { // zmienic nazwe xd
+        if (poz.y > wysokośćPlanszy - 1)
+        {
+            return null;
+        }
+        else { }
+    }
+        public void spawnNowegoKlocka()
     {
         GameObject nowyKlocek = (GameObject)Instantiate(Resources.Load(pobierzRandomowyKlocek(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);
     }
     public bool sprawdzCzyJestWPlanszy(Vector3 pozycja)
     {
-        return ((int) pozycja.x >= 0 && (int)pozycja.x < szerokośćPlanszy && (int)pozycja.y >= 0);
+        return ((int)pozycja.x >= 0 && (int)pozycja.x < szerokośćPlanszy && (int)pozycja.y >= 0);
     }
 
-    public Vector2 Round (Vector2 pozycja)
+    public Vector2 Round(Vector2 pozycja)
     {
-        return new Vector2 (Mathf.Round(pozycja.x), Mathf.Round(pozycja.y));
+        return new Vector2(Mathf.Round(pozycja.x), Mathf.Round(pozycja.y));
     }
 
     string pobierzRandomowyKlocek()
     {
         int randomKlocek = Random.Range(1, 8);
         string nazwaRandomowegoKlocka = "Prefabs/T";
-        
+
         switch (randomKlocek)
         {
             case 1:
--- Assets/Scripts/Klocek.cs
+++ Assets/Scripts/Klocek.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Klocek : MonoBehaviour {
+public class Klocek : MonoBehaviour
+{
 
     float fall = 0;
     public float fallSpeed = 1;
     public bool dopuśćRotacje = true;
     public bool ogarniczRotacje = false;
     // Use this for initialization
-    void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Sterowanie();
-	}
+    }
     public void Sterowanie()
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
@@ -24,8 +27,9 @@ public class Klocek : MonoBehaviour {
             transform.position += new Vector3(1, 0, 0);
             if (sprawdzCzyJestWDobrejPozycji())
             {
-
-            }else
+               FindObjectOfType<Gra>().aktualizowanieSiatki(this); // tu tez cos zmienialem xd
+            }
+            else
             {
                 transform.position += new Vector3(-1, 0, 0);
             }
@@ -46,7 +50,8 @@ public class Klocek : MonoBehaviour {
         {
             if (dopuśćRotacje)
             {
-                if (ogarniczRotacje){
+                if (ogarniczRotacje)
+                {
 
                     if (transform.rotation.eulerAngles.z >= 90)
                     {
@@ -56,14 +61,15 @@ public class Klocek : MonoBehaviour {
                     {
                         transform.Rotate(0, 0, 90);
                     }
-                    
-                }else
+
+                }
+                else
                 {
                     transform.Rotate(0, 0, 90);
                 }
                 if (sprawdzCzyJestWDobrejPozycji())
                 {
-
+                    FindObjectOfType<Gra>().aktualizowanieSiatki(this); //tu tez
                 }
                 else
                 {
@@ -77,21 +83,22 @@ public class Klocek : MonoBehaviour {
                         {
                             transform.Rotate(0, 0, 90);
                         }
-                    }else
-                    {
-                        transform.Rotate(0,0,-90);
                     }
-                    
+                    else
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+
                 }
             }
-            
+
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
         {
-            transform.position += new Vector3(0,-1,0);
+            transform.position += new Vector3(0, -1, 0);
             if (sprawdzCzyJestWDobrejPozycji())
             {
-
+                FindObjectOfType<Gra>().aktualizowanieSiatki(this); // tuuu
             }
             else
             {
@@ -101,7 +108,8 @@ public class Klocek : MonoBehaviour {
             }
             fall = Time.time;
         }
-    }bool sprawdzCzyJestWDobrejPozycji()
+    }
+    bool sprawdzCzyJestWDobrejPozycji()
     {
         foreach (Transform klocek in transform)
         {
@@ -110,7 +118,9 @@ public class Klocek : MonoBehaviour {
             {
                 return false;
             }
-        }
+            if (FindObjectOfType<Gra>().GetTransformGridPosition(poz) != null & FindObjectOfType<Gra>().GetTransformGridPosition(poz).parent != transform) // tu tez zmienic nazwe na to co tam
+                return false;
+          }
         return true;
     }
 
