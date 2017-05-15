8c88c9db175df7856f5d54aacdfc516d055e601f
lewkaj <naex1337@gmail.com>
2017-05-15 16:16:49 +0200

    menu, trzymanie przyciskow, troche audio

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
-
+	public void NowaGraB(string Level)
+	{
+		SceneManager.LoadScene (Level);
+	}
+	public void WyjdzB()
+	{
+		Application.Quit ();
+	}
     public void ZagrajPonownie()
     {
         Application.LoadLevel("Level");
